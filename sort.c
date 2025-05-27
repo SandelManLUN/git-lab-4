23e7cff77413131da6189d0252f0301a66899b0e
42bratuha <ilarovicfedor@gmail.com>
2025-05-27 19:47:55 +0700

    Update sort.c with main and fix array

--- sort.c
+++ sort.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void bubble_sort(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -9,4 +11,9 @@ void bubble_sort(int arr[], int n) {
         }
     }
 }
-// Sorting array example
+
+int main() {
+    int a[] = {4, 0};
+    bubble_sort(a, 3);
+    return 0;
+}
